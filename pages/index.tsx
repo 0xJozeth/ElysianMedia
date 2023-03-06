f46cef0c643844bfd566110aa0265fff2d4e0308
Glen <0xJozeth@gmail.com>
2023-03-06 13:30:21 -0500

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,20 +15,22 @@ const archivo = Archivo_Black({
 const Home: NextPage = () => {
 	return (
 		<>
-			<nav className='absolute w-[600px] top-[50%] -rotate-90 -right-[200px] uppercase z-50'>
-				<ul className='flex w-full h-full text-black justify-between'>
-					<Link href='contact'>Contact</Link>
-					<Link href='services'>Services</Link>
-					<Link href='works'>Works</Link>
-					<Link href='home'>Home</Link>
-				</ul>
-			</nav>
+			<div className='sidebar'>
+				<nav className='absolute w-[600px] top-[50%] -rotate-90 -right-[200px] uppercase z-50'>
+					<ul className='flex w-full h-full text-black text-[20px] pt-8 justify-between'>
+						<Link href='contact'>Contact</Link>
+						<Link href='services'>Services</Link>
+						<Link href='works'>Works</Link>
+						<Link href='home'>Home</Link>
+					</ul>
+				</nav>
+			</div>
 			<main className={archivo.className}>
 				<section className='flex flex-col h-screen w-screen mx-auto'>
 					<Image src={elysianLogo} alt='' className='absolute z-50 p-8' />
 					<Link
 						href='/contact'
-						className='absolute flex right-24 top-8 p-8 bg-black text-white text-[24px] h-2 w-56 items-center justify-center z-30'
+						className='absolute flex right-36 top-8 p-8 bg-black text-white text-[24px] h-2 w-56 items-center justify-center z-30'
 					>
 						Hire Us
 					</Link>
